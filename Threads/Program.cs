0e69dc3d05c18a476c8cb9af01c7bad95c709dda
Adeliyaa <47208186+Adeliyaa@users.noreply.github.com>
2019-03-29 09:44:09 +0600

    Add files via upload

---
+++ Threads/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadsE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Print1);
+
+            t.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(0);
+                Thread.Sleep(0);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void Print1()
+        {
+            for(int i=0; i<10; i++)
+            {
+                Console.Write(1);
+                Thread.Sleep(0);
+            }
+        }
+    }
+
+   
+
+}
