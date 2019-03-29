6eeeb019b87d66c78ee0374c74470b45880d886b
Adeliyaa <47208186+Adeliyaa@users.noreply.github.com>
2019-03-29 09:42:26 +0600

    Add files via upload

---
+++ ThreadExample1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(DoIt);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            
+
+            DoIt();
+            DoIt();
+        }
+
+        private static void DoIt()
+        {
+            while(true)
+           {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+           }
+        }
+    }
+}
