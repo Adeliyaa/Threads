11feb5766723ef2297115a5ceea2f9520c3499b1
Adeliyaa <47208186+Adeliyaa@users.noreply.github.com>
2019-03-29 09:45:05 +0600

    Add files via upload

---
+++ ThreadsE2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadsE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Print);
+            t.Start();
+            int num = 1;
+            for(int i=0; i<10; i++)
+            {
+                Console.WriteLine(num);
+                Thread.Sleep(1000);
+                num++;
+            }
+            Console.WriteLine("Threads Ends");
+        }
+
+        static void Print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(123);
+                Thread.Sleep(2000);
+                
+            }
+
+        }
+
+    }
+}
