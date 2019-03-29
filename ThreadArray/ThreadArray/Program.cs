febdb7f6e2b5998e1e2e01f2e3b4449a8fc7d962
Adeliyaa <47208186+Adeliyaa@users.noreply.github.com>
2019-03-29 09:40:57 +0600

    Add files via upload

---
+++ ThreadArray/ThreadArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread[] threadsArray = new Thread[4];
+            for (int i=0; i<4; i++)
+            {
+                int localNum = i;
+                threadsArray[i] = new Thread(() => k(localNum));
+            }
+
+            for(int i=0; i<4; i++)
+            {
+                threadsArray[i].Start();
+                Thread.Sleep(1000);
+            } 
+
+            for(int i=0; i<4; i++)
+            {
+                threadsArray[i].Join();
+            }
+
+            void k(int i)
+            {
+              
+                Console.WriteLine(i);
+               
+
+            }
+        }
+
+        
+    }
+}
